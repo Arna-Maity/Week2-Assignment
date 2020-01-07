44775786c624ec5cb23593c67c511f88123197f1
Arna-Maity <arnamaity6230@gmail.com>
2020-01-07 09:00:52 -0700

    Improved functionlity of build system.

--- src/sources.mk
+++ src/sources.mk
@@ -11,14 +11,8 @@
 
 # Add your Source files to this variable
 SOURCES = main.c \
-	  memory.c \
-	  interrupts_msp432p401r_gcc.c \
-	  startup_msp432p401r_gcc.c \
-	  system_msp432p401r.c
+	  memory.c 
 
 # Add your include paths to this variable
-INCLUDES = ../include/common/
+INCLUDES = -I../include/common/ -I../include/msp432/ -I../include/CMSIS/
 
-INCLUDES_MSP432 = ../include/msp432/ 
-
-INCLUDES_CMSIS = ../include/CMSIS/
